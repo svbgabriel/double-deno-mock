6d0c20e3a130d5f2a5018c0a5cec497d490ebe24
Gabriel Batista <svbgabriel@proton.me>
2026-08-07 20:54:23 -0300

    test: enhance assertions with assertExists and assertArrayIncludes in REST tests

--- tests/rest.test.ts
+++ tests/rest.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from '@std/assert'
+import { assertArrayIncludes, assertEquals, assertExists } from '@std/assert'
 import { MatchingEngine } from '../src/mocks/engine.ts'
 import { Mock, MockStore } from '../src/mocks/types.ts'
 
@@ -74,7 +74,8 @@ Deno.test('REST Responder - Collection POST', async () => {
   // Verify persistence
   assertEquals(store.lastUpdatedId, 'rest-1')
   assertEquals(store.lastUpdatedState?.length, 1)
-  assertEquals((store.lastUpdatedState as any[])[0].name, 'New Item')
+  assertExists(store.lastUpdatedState)
+  assertArrayIncludes(store.lastUpdatedState, [{ id: body.id, name: body.name }])
 })
 
 Deno.test('REST Responder - Item GET/PUT/PATCH/DELETE', async () => {
@@ -111,9 +112,11 @@ Deno.test('REST Responder - Item GET/PUT/PATCH/DELETE', async () => {
     body: JSON.stringify({ name: 'Updated 1' })
   }))
   assertEquals(resPut?.status, 200)
-  assertEquals(JSON.parse(resPut?.body || '{}').name, 'Updated 1')
-  assertEquals(JSON.parse(resPut?.body || '{}').id, '1')
-  assertEquals((store.lastUpdatedState as any[])[0].name, 'Updated 1')
+  const putBody = JSON.parse(resPut?.body || '{}')
+  assertEquals(putBody.name, 'Updated 1')
+  assertEquals(putBody.id, '1')
+  assertExists(store.lastUpdatedState)
+  assertArrayIncludes(store.lastUpdatedState, [{ id: putBody.id, name: putBody.name }])
 
   // PATCH
   const resPatch = await engine.handleRequest(new Request('http://localhost/items/2', {
@@ -125,7 +128,8 @@ Deno.test('REST Responder - Item GET/PUT/PATCH/DELETE', async () => {
   const patchBody = JSON.parse(resPatch?.body || '{}')
   assertEquals(patchBody.name, 'Item 2')
   assertEquals(patchBody.description, 'New field')
-  assertEquals((store.lastUpdatedState as any[])[1].description, 'New field')
+  assertExists(store.lastUpdatedState)
+  assertArrayIncludes(store.lastUpdatedState, [{ id: patchBody.id, description: patchBody.description, name: patchBody.name }])
 
   // DELETE
   const resDelete = await engine.handleRequest(new Request('http://localhost/items/1', {
@@ -133,7 +137,7 @@ Deno.test('REST Responder - Item GET/PUT/PATCH/DELETE', async () => {
   }))
   assertEquals(resDelete?.status, 204)
   assertEquals(store.lastUpdatedState?.length, 1)
-  assertEquals((store.lastUpdatedState as any[])[0].id, '2')
+  assertArrayIncludes(store.lastUpdatedState, [{ id: '2', name: 'Item 2', description: 'New field' }])
 })
 
 Deno.test('REST Responder - Custom id field', async () => {
