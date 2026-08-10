9161d727362b41b073fcd5d86e6053e9fe2c24ba
Gabriel Batista <svbgabriel@proton.me>
2026-08-09 21:30:32 -0300

    chore: add docker-compose for local dev and update default PostgreSQL credentials

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const PORT = Number(Deno.env.get('PORT') || '8000')
 export const DB_DRIVER = Deno.env.get('DB_DRIVER') ?? 'memory' // postgres or memory
-export const POSTGRES_URL = Deno.env.get('POSTGRES_URL') ?? 'postgres://postgres:postgres@localhost:5432/mocks'
+export const POSTGRES_URL = Deno.env.get('POSTGRES_URL') ?? 'postgres://mock:mock@localhost:5432/mocks'
 export const scriptTimeoutMs = Number(Deno.env.get('SCRIPT_TIMEOUT_MS') || '2000')
